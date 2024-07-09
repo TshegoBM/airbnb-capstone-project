f85dd51c1eb51e5e115d38988d0fb929f3493c45
“Tshegofatso <motsuenyanetshegofatso@gmail.com>
2024-07-09 11:25:20 +0200

    HeroBanner update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeroBanner from './components/HomePage/HeroBanner';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroBanner />
+    </div>
+  );
+}
+
+export default App;
